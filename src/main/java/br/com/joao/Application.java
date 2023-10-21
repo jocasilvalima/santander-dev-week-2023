a352aa3f444829493f80d7870464b75c1424dc91
João Carllos <jocasilvalima@gmail.com>
2023-10-20 21:19:17 -0300

    Bugfix CORS

--- src/main/java/br/com/joao/Application.java
+++ src/main/java/br/com/joao/Application.java
@@ -1,8 +1,13 @@
 package br.com.joao;
 
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Service;
 
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 public class Application {
 
